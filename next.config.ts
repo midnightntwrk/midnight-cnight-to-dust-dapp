b17e50ab2242d11763fb1810f43c7468bb9f882f
Vanessa Rodriguez Cristobal <vanessa.cristobal@iohk.io>
2026-03-04 09:25:16 +0000

    chore/update dockerfile

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,6 @@ const nextConfig: NextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   poweredByHeader: false,
-  serverExternalPackages: [
-    'libsodium-wrappers-sumo',
-    'libsodium-sumo',
-    '@emurgo/cardano-message-signing-nodejs',
-    '@emurgo/cardano-message-signing-browser',
-    '@midnight-ntwrk/ledger-v7',
-    '@midnight-ntwrk/zswap',
-    '@lucid-evolution/uplc',
-    '@anastasia-labs/cardano-multiplatform-lib-nodejs',
-    '@anastasia-labs/cardano-multiplatform-lib-browser',
-    '@blaze-cardano/uplc',
-    '@cardano-sdk/crypto',
-  ],
   webpack: (config) => {
     config.experiments = {
       ...config.experiments,
